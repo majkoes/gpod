4ab42482fe51f6a328b7b7ebc5244f5e3d3b4c4d
Marian Suchodolsky <marian.suchodolsky@gmail.com>
2017-08-10 11:00:46 +0200

    fdf

--- protractor.conf.js
+++ protractor.conf.js
@@ -2,6 +2,7 @@
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
 const { SpecReporter } = require('jasmine-spec-reporter');
+const path = require('path');
 
 exports.config = {
   allScriptsTimeout: 11000,
@@ -12,7 +13,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'http://'+require("os").hostname() + ':4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
