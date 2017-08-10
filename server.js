d7d040e50f9e34fce183458dc61dcfbc856485e5
Marian Suchodolsky <marian.suchodolsky@gmail.com>
2017-08-10 16:41:26 +0200

    245

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-const path = require('path');
-
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || 'http://' + require("os").hostname() + server_port
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
 server.listen(server_port, server_ip_address, function(){
   console.log("Listening on " + server_ip_address + ", server_port " + server_port)
